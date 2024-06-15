8619ebe97545d8b3cadbcc41e84a0720771b37b4
Simon Farshid <simon.farshid@outlook.com>
2024-06-15 14:38:44 -0700

    fix height

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full">
 			<body className={cn(inter.className, "h-full")}>
 				<MyRuntimeProvider>{children}</MyRuntimeProvider>
 			</body>
