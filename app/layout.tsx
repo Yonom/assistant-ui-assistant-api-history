606e3bb453d06d8e709b2b564bc9de5cbbe34922
Simon Farshid <simon.farshid@outlook.com>
2024-06-15 14:38:35 -0700

    Fix height

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { MyRuntimeProvider } from "./MyRuntimeProvider";
 
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
+			<body className={cn(inter.className, "h-full")}>
 				<MyRuntimeProvider>{children}</MyRuntimeProvider>
 			</body>
 		</html>
